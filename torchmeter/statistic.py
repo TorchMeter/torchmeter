b79c364ba3ccba69679def866c5da8ed5066b7a4
Ahzyuan <hzy_dev@yeah.net>
2025-09-24 14:56:46 +0800

    refactor: delete trailing spaces in tp calculating line

--- torchmeter/statistic.py
+++ torchmeter/statistic.py
@@ -868,7 +868,7 @@ class IttpMeter(Statistics):
                     cuda_sync()  # WAIT FOR GPU SYNC
                     it = start_event.elapsed_time(end_event) * 1e-3  # ms -> s    # type: ignore
 
-                tp = 1 / it  
+                tp = 1 / it
                 self.__InferTime.append(it)
                 self.__Throughput.append(tp)
 
